5e56c76726a0d04801eb230e092290f352d461ea
ryosuke1202 <ryosuke12021@gmail.com>
2023-02-03 20:33:11 +0900

    TODOアプリ

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,85 @@
 import React, { useState } from 'react';
-import { useEffect } from 'react';
-import ColorfullMessage from './components/ColorfullMessage';
+import './App.css';
+import { InputTodo } from './components/InputTodo';
+import { InCompleteTodos } from './components/InCompleteTodos';
+import { CompleteTodos } from './components/CompleteTodos';
+import { Home } from './components/Home';
+import { Contact } from './components/Contact';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+// import { useEffect } from 'react';
 
-const App = () => {
-  const [num, setNum] = useState(0);
-  const [faseFlg, setFaseFlg] = useState(true);
+export const App = () => {
+  const [todoText, setTodoText] = useState('');
+  const [incompleteTodls, setIncompleteTodls] = useState(['あああ', 'いいいい']);
+  const [completeTodls, setCompleteTodls] = useState(['ううう', 'いいええええいい']);
 
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
-  const onClickChangeFlg = () => {
-    setFaseFlg(!faseFlg);
+  const onChangeTodoText = (event) => setTodoText(event.target.value)
+
+  // 追加ボタン
+  const onClickAdd = () => {
+    if (todoText === '') {
+      alert('入力して下さい！');
+      return;
+    }
+    const newTodos = [...incompleteTodls, todoText];
+    setIncompleteTodls(newTodos);
+    // incompleteTodls.push(todoText);
+    // setIncompleteTodls([...incompleteTodls]);
+    setTodoText('');
   }
-  useEffect(() => {
-    if (num % 3 === 0) {
-      faseFlg || setFaseFlg(true);
-    }
-    if (num % 3 !== 0) {
-      faseFlg && setFaseFlg(false);
-    }
-  }, [num]);
+  // 未完了のTODOの削除ボタン
+  const onClickDelete = (index) => {
+    const todos = [...incompleteTodls];
+    todos.splice(index, 1);
+    setIncompleteTodls(todos);
+  }
+  // 未完了のTODOの完了ボタン
+  const onClickComplete = (index) => {
+    // 完了したTODOへ追加し未完了のRODOから削除
+    const completeTodo = incompleteTodls[index];
+    const todos = [...incompleteTodls];
+    const newComplete = [...completeTodls, completeTodo];
+    setCompleteTodls(newComplete);
+    todos.splice(index, 1);
+    setIncompleteTodls(todos);
+
+  }
+  // 完了したTODOの戻すボタン
+  const onClickBackTodo = (index) => {
+    // 完了TODOから削除
+    const todos = [...completeTodls];
+    const backTodo = todos[index];
+    todos.splice(index, 1);
+    setCompleteTodls(todos);
+    // 未完了TODOへ追加
+    const newTodos = [...incompleteTodls, backTodo];
+    setIncompleteTodls(newTodos);
+  }
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
-      <ColorfullMessage color="pink">元気です</ColorfullMessage>
-      <button onClick={onClickCountUp}>カウントアップ</button><br />
-      <button onClick={onClickChangeFlg}>on/off</button>
-      <p>{num}</p>
-      { faseFlg && <p>顔文字</p> }
-    </>
-  );
-}
+    <BrowserRouter>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contact">contact</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route>
+          <p>Not Found</p>
+        </Route>
+      </Switch>
+      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd}/>
+      
+      <InCompleteTodos incompleteTodls={incompleteTodls} onClickComplete={onClickComplete} onClickDelete={onClickDelete} />
 
-export default App;
+      <CompleteTodos completeTodls={completeTodls} onClickBackTodo={onClickBackTodo} />
+    </BrowserRouter>
+    </>
+  )
+}
