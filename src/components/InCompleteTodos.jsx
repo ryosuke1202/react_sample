8712503b0690318e60f0ab45e340563dc96830c0
ryosuke1202 <ryosuke12021@gmail.com>
2023-02-04 16:00:28 +0900

    エラー文を追加

--- src/components/InCompleteTodos.jsx
+++ src/components/InCompleteTodos.jsx
@@ -5,6 +5,7 @@ export const InCompleteTodos = (props) => {
     return (
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
+        { incompleteTodls.length >= 5 && <p className="text-red">未完了のTODOの登録は5つまでです。</p> }
         <ul id="incomplete-list">
           {incompleteTodls.map((todo, index) => {
             return (
